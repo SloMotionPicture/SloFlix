06ab2cf6c3787b033d7d752de30fa35dea3c74a3
Bryan <template.privacy.policy@gmail.com>
2019-01-09 14:09:47 -0800

    More layout

---
+++ client/components/AdView.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+
+const AdView = () => {
+  return <div className="ad_View" />
+}
+
+export default AdView
