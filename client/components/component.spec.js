33c23d501b8a874721389e8f6eace3c7a7e600bd
Bryan <template.privacy.policy@gmail.com>
2019-01-09 15:05:21 -0800

    Basic Layout; forgot to switch branches when writing tests

---
+++ client/components/component.spec.js
@@ -0,0 +1,40 @@
+/* global describe beforeEach it */
+
+import {expect} from 'chai'
+import React from 'react'
+import enzyme, {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import {MovieView} from '../components'
+import {Provider} from 'react-redux'
+import store from '../store'
+import moduleName from 'module'
+const adapter = new Adapter()
+enzyme.configure({adapter})
+
+describe('Components', () => {
+  let movieView
+
+  beforeEach(() => {
+    movieView = shallow(
+      <Provider store={store}>
+        <MovieView />
+      </Provider>
+    )
+  })
+
+  it('renders a movie title', () => {
+    expect('testing').to.not.equal('testing')
+  })
+  it('renders a movie image', () => {
+    expect('testing').to.not.equal('testing')
+  })
+  it('renders movies into a grid of cells', () => {
+    expect('testing').to.not.equal('testing')
+  })
+  it('renders all tags in the LeftSideBar', () => {
+    expect('testing').to.not.equal('testing')
+  })
+  it('has Login and SignUp buttons with classNames `login` and`signUp` respectively', () => {
+    expect('testing').to.not.equal('testing')
+  })
+})
