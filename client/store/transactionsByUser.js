f54978b29f43f701ed6d31c0261a3624baa335ce
Scott Anderson <scottandersongis@gmail.com>
2019-01-09 14:04:44 -0800

    added more objects to redux store

---
+++ client/store/transactionsByUser.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import history from '../history'
+
+/**
+ * ACTION TYPES
+ */
+const GET_TRANSACTIONS_BY_USER = 'GET_TRANSACTIONS_BY_USER'
+
+/**
+ * INITIAL STATE
+ */
+const defaultTransactionByUser = []
+
+/**
+ * ACTION CREATORS
+ */
+export const getTransactionsByUser = userTransactions => ({
+  type: GET_TRANSACTIONS_BY_USER,
+  userTransactions
+})
+
+/**
+ * THUNK CREATORS
+ */
+export const fetchTransactionsByUser = userId => async dispatch => {
+  try {
+    const response = await axios.get(`/api/transactions/${userId}`)
+    const action = getTransactionsByUser(response.data)
+    dispatch(action)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+/**
+ * REDUCER
+ */
+
+export default function(state = defaultTransactionByUser, action) {
+  switch (action.type) {
+    case GET_TRANSACTIONS_BY_USER:
+      return defaultTransactionByUser
+    default:
+      return state
+  }
+}
