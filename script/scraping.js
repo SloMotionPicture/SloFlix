51c6bfceda0561b1ffd7a5e6ffe59d13945e068a
Bryan <template.privacy.policy@gmail.com>
2019-01-08 16:55:51 -0800

    Scraped data is correctly formatted

--- script/scraping.js
+++ script/scraping.js
@@ -1,66 +1,90 @@
 const cheerio = require('cheerio')
 const request = require('request')
 const scrapeIt = require('scrape-it')
-let movies = require('./sample_Movie_Object').data.movies
-let url = 'https://www.imdb.com/list/ls057823854/'
-console.log('STARTING')
-
-let firstMovie = movies[0]
-firstMovie.tags = firstMovie.tags[0].split(',')
-let arr = firstMovie.description.split('\n')
-console.log(arr)
-const description = arr[7]
-let director = []
-let actors = []
-let index = 9
-while (arr[index] !== '                 | ') {
-  let currentDirector = arr[index]
-  const commaindex = currentDirector.indexOf(',')
-  if (commaindex !== -1) {
-    const actual = currentDirector.slice(0, commaindex)
-    director.push(actual)
-  } else {
-    director.push(currentDirector)
+let url =
+  'https://www.imdb.com/list/ls057823854/?sort=list_order,asc&st_dt=&mode=detail&page=94'
+const start = async () => {
+  const fetchMovies = async () => {
+    console.log('...STARTING...')
+    return scrapeIt(
+      url,
+      {
+        movies: {
+          listItem: '.lister-item',
+          data: {
+            title: '.lister-item-header>a',
+            year: '.lister-item-year',
+            imageUrl: {
+              selector: '.lister-item-image>a>img',
+              attr: 'src'
+            },
+            rating:
+              '.ipl-rating-widget>.ipl-rating-star>.ipl-rating-star__rating',
+            certificate: '.certificate',
+            runtime: '.runtime',
+            tags: {
+              listItem: '.genre'
+            },
+            summary: '.lister-item-content>p'
+          }
+        }
+      },
+      (err, data) => {
+        console.log(err || 'SUCCESS')
+        return data
+      }
+    )
   }
-  index++
+  let data = await fetchMovies()
+  let movies = data.data.movies
+  movies.map(movie => {
+    if (movie.tags[0] !== undefined) {
+      movie.tags = movie.tags[0].split(',')
+    }
+    let arr = movie.summary.split('\n')
+    const summary = arr[7]
+    let director = []
+    let actors = []
+    let index = 9
+    while (arr[index] !== '                 | ') {
+      let currentDirector = arr[index]
+      if (currentDirector !== undefined) {
+        const commaindex = currentDirector.indexOf(',')
+        if (commaindex !== -1) {
+          const actual = currentDirector.slice(0, commaindex)
+          director.push(actual)
+        } else {
+          director.push(currentDirector)
+        }
+        index++
+      } else {
+        break
+      }
+    }
+    index++
+    index++
+    while (arr[index] !== '    ') {
+      let currentActor = arr[index]
+      if (currentActor !== undefined) {
+        const commaindex = currentActor.indexOf(',')
+        if (commaindex !== -1) {
+          const actual = currentActor.slice(0, commaindex)
+          actors.push(actual)
+        } else {
+          actors.push(currentActor)
+        }
+        index++
+      } else {
+        break
+      }
+    }
+    movie.summary = summary
+    movie.cast = actors
+    movie.director = director
+    return movie
+  })
+  console.log(movies)
+  return movies
 }
-index++
-index++
-while (arr[index] !== '    ') {
-  let currentActor = arr[index]
-  const commaindex = currentActor.indexOf(',')
-  if (commaindex !== -1) {
-    const actual = currentActor.slice(0, commaindex)
-    actors.push(actual)
-  } else {
-    actors.push(currentActor)
-  }
-  index++
-}
-firstMovie.description = description
-firstMovie.actors = actors
-firstMovie.director = director
-console.log('HERE', firstMovie)
 
-// scrapeIt(url, {
-//   movies: {
-//     listItem: ".lister-item",
-//     data: {
-//       title: ".lister-item-header>a",
-//       year: ".lister-item-year",
-//       img: {
-//         selector: ".lister-item-image>a>img",
-//         attr: "src"
-//       },
-//       rating: ".ipl-rating-widget>.ipl-rating-star>.ipl-rating-star__rating",
-//       certificate: ".certificate",
-//       runtime: ".runtime",
-//       tags: {
-//         listItem: ".genre"
-//       },
-//       description: ".lister-item-content>p"
-//     }
-//   }
-// }, (err, data) => {
-//   console.log(err || 'SUCCESS', data);
-// })
+start()
