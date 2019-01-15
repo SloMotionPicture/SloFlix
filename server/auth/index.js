7ac14ea0e23fb74e4617131a364bd3dd6cee662c
Scott Anderson <scottandersongis@gmail.com>
2019-01-15 15:04:52 -0800

    Error with signin - Fixed

--- server/auth/index.js
+++ server/auth/index.js
@@ -21,7 +21,8 @@ router.post('/login', async (req, res, next) => {
 
 router.post('/signup', async (req, res, next) => {
   try {
-    const user = await User.create(req.body)
+    let newUser = req.body
+    const user = await User.create(newUser)
     req.login(user, err => (err ? next(err) : res.json(user)))
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
